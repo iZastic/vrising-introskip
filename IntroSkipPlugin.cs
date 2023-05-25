f1763bb96ed3181e48e9ba3c0236aad9f69b0789
iZastic <izastic@gmail.com>
2023-05-25 02:51:10 -0600

    Initial commit

---
+++ IntroSkipPlugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using ProjectM.UI;
+
+namespace IntroSkip
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class IntroSkipPlugin : BasePlugin
+    {
+        private static ManualLogSource Logger;
+        private Harmony harmony;
+
+        public override void Load()
+        {
+            Logger = Log;
+            harmony = Harmony.CreateAndPatchAll(typeof(IntroSkipPlugin));
+
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StartGameLoadMenuView), nameof(StartGameLoadMenuView.Update))]
+        private static void UpdatePrefix(StartGameLoadMenuView __instance)
+        {
+            if (__instance.VideoPlayer.isPlaying)
+            {
+                __instance.VideoPlayer.Stop();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# IntroSkip
+Automatically skips the intro video when starting the game
+
+### Installation
+- Install [BepInEx](https://v-rising.thunderstore.io/package/BepInEx/BepInExPack_V_Rising/)
+- Extract _IntroSkip.dll_ into _(VRising folder)/BepInEx/plugins_
+
+### Support
+Join the [modding discord](https://vrisingmods.com/discord) for support and tag `@iZastic#0365`
+
+Submit a ticket on [GitHub](https://github.com/iZastic/vrising-introskip/issues)
+
+### Changelog
+`1.0.0`
+- Initial release
+
+</details>
